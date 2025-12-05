d5ebd2b38061f57f24019bb2ae1e08d10b40e0e0
untildead <angel.ortiz102878@gmail.com>
2025-12-05 08:54:45 -0500

    Modelo Jugador y Partido

--- models/models.py
+++ models/models.py
@@ -1,12 +1,32 @@
-class Jugador():
-    pass
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
+from datetime import date
 
+class Jugador(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nombre: str
+    numero: int
+    fecha_nacimiento: date
+    nacionalidad: str
+    altura: float
+    peso: float
+    posicion: str
+    pie_dominante: str
+    estado: str
+    estadisticas_acumuladas: str = "0 goles, 0 asistencias"
 
-class Estadistica():
-    pass
+    partidos: List["Partido"] = Relationship(back_populates="jugador")
 
+class Partido(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    fecha: date
+    rival: str
+    condicion: str
+    goles: int
+    tarjetas: int
+    faltas: int
+    minutos_jugados: int
+    resultado: str
 
-class Partido():
-    pass
-
-
+    jugador_id: int = Field(foreign_key="jugador.id")
+    jugador: Jugador = Relationship(back_populates="partidos")
